7c516d52ad40f95dbc39457e618a22fe72ebb4c6
abhishek goswami <abhishek.goswami@pipedrive.com>
2021-01-30 13:23:39 +0200

    move things around

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,13 @@
+vim.cmd [[packadd vimball]]
+
 require('settings')
 require('plugins')
 require('mappings')
+
+require('hiro.globals')
+
+require('hiro.lsp')
+require('hiro.treesitter')
+
+require('hiro.telescope')
+require('hiro.telescope.mappings')
--- nvim/lua/hiro/globals/init.lua
+++ nvim/lua/hiro/globals/init.lua
@@ -9,9 +9,3 @@ P = function(v)
   print(vim.inspect(v))
   return v
 end
-
-require('hiro.lsp')
-require('hiro.treesitter')
-
-require('hiro.telescope')
-require('hiro.telescope.mappings')
