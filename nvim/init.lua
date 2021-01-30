75c849824bc0f2ee7414b432f3e511068afe3b52
abhishek goswami <abhishek.goswami@pipedrive.com>
2021-01-30 14:22:53 +0200

    move globals.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,12 @@
 vim.cmd [[packadd vimball]]
 
+require('hiro.globals')
+
+-- load packer.nvim files
 require('hiro.plugins')
+
 require('settings')
 require('mappings')
-
-require('hiro.globals')
 
 require('hiro.lsp')
 require('hiro.treesitter')
